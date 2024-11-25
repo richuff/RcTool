2b0609d9a93a6ec6559808515a4706347677816c
richu <1691401076@qq.com>
2024-11-25 18:28:54 +0800

    chore:fixed the bug

--- lib/routers/index.dart
+++ lib/routers/index.dart
@@ -1,10 +1,17 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:rctool/page/AboutAuthor.dart';
 import 'package:rctool/page/MusicPage.dart';
 
 var routes = [
   GetPage(
       name: "/music",
       page: () => const MusicPage(),
+      transition: Transition.rightToLeft
+  ),
+
+  GetPage(
+      name: "/about",
+      page: () => const AboutAuthor(),
       transition: Transition.rightToLeft)
 ];
