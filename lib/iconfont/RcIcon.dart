035af57ef364789844f67d13182fedc12bd594c6
richu <1691401076@qq.com>
2025-01-03 21:42:40 +0800

    chore:add iconfont

---
+++ lib/iconfont/RcIcon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class RcIcon{
+  static IconData check = const IconData(
+      0xe645,   //unicode编码
+      fontFamily: "richuicon",   //自定义的fontFamily
+      matchTextDirection: true
+  );
+
+  static const IconData close = IconData(
+      0xe646,   //unicode编码
+      fontFamily: "richuicon",   //自定义的fontFamily
+      matchTextDirection: true
+  );
+}
