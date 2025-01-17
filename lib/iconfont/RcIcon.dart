a62ced3b45b4270ff8f574bdc2d6b761113caa42
richu <1691401076@qq.com>
2025-01-17 14:12:20 +0800

    fixed:add the iconfont

--- lib/iconfont/RcIcon.dart
+++ lib/iconfont/RcIcon.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/cupertino.dart';
 
 class RcIcon{
-  static IconData check = const IconData(
-      0xe645,   //unicode编码
-      fontFamily: "richuicon",   //自定义的fontFamily
+  static const IconData english = const IconData(
+      0xec6a,   //unicode编码
+      fontFamily: "RcIcon",
       matchTextDirection: true
   );
 
-  static const IconData close = IconData(
-      0xe646,   //unicode编码
-      fontFamily: "richuicon",   //自定义的fontFamily
+  static const IconData chinese = IconData(
+      0xe6c4,   //unicode编码
+      fontFamily: "RcIcon",
       matchTextDirection: true
+  );
+  static const IconData japanese = IconData(
+      0xec69,   //unicode编码
+      fontFamily: "RcIcon",
+      matchTextDirection: true
+  );
+  static const IconData livetod = IconData(
+      0xe700, //unicode编码
+    fontFamily: "RcIcon",
+    matchTextDirection: true
   );
 }
