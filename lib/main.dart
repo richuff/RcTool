c19de93a78f93da363b32f3dad2043dbbe550c7f
richu <1691401076@qq.com>
2025-01-17 14:12:45 +0800

    chore:change the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:rctool/routers/index.dart';
+import 'package:rctool/utils/Message.dart';
+import 'package:rctool/utils/NotificationHelper.dart';
 import 'package:rctool/widget/SearchWidget.dart';
 
-void main() {
+import 'iconfont/RcIcon.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final notificationHelper = NotificationHelper();
+  await notificationHelper.initialize();
   runApp(const MyApp());
 }
 
@@ -16,21 +23,38 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyApp();
 }
 
-class _MyApp extends State<MyApp> {
+class _MyApp extends State<MyApp> with SingleTickerProviderStateMixin {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'RcTool',
-      debugShowCheckedModeBanner: false,
-      getPages: routes,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
-        useMaterial3: true,
-      ),
-      initialRoute: "/",
-      home: Scaffold(
+        title: 'RcTool',
+        debugShowCheckedModeBanner: false,
+        getPages: routes,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
+          useMaterial3: true,
+        ),
+        initialRoute: "/",
+        translations: Message(),
+        locale: const Locale('zh', 'CN'), //指定默认的语言
+        fallbackLocale: const Locale('en', 'US'), //添加一个回调语言选项
+        home: Scaffold(
           key: scaffoldKey,
           drawer: Drawer(
             width: 250,
@@ -48,9 +72,9 @@ class _MyApp extends State<MyApp> {
                           Container(
                             height: 40.0, // 设置高度
                             alignment: Alignment.center,
-                            child: const Text(
-                              '主页',
-                              style: TextStyle(
+                            child: Text(
+                              '主页'.tr,
+                              style: const TextStyle(
                                 color: Colors.white,
                                 fontSize: 24,
                               ),
@@ -61,15 +85,23 @@ class _MyApp extends State<MyApp> {
                 ),
                 ListTile(
                   leading: const Icon(Icons.music_note),
-                  title: const Text('听音乐'),
+                  title: Text('听音乐'.tr),
                   onTap: () {
                     scaffoldKey.currentState?.closeDrawer();
                     Get.toNamed("/music");
                   },
                 ),
                 ListTile(
+                  leading: const Icon(RcIcon.livetod),
+                  title: Text('live2d'.tr),
+                  onTap: () {
+                    scaffoldKey.currentState?.closeDrawer();
+                    Get.toNamed("/livetod");
+                  },
+                ),
+                ListTile(
                   leading: const Icon(Icons.account_box_rounded),
-                  title: const Text('关于作者'),
+                  title: Text('关于作者'.tr),
                   onTap: () {
                     scaffoldKey.currentState?.closeDrawer();
                     Get.toNamed("/about");
@@ -100,18 +132,98 @@ class _MyApp extends State<MyApp> {
                 ]);
               }),
               actions: [SearchWidget()]),
-          body: Column(
-            children: [
-              ElevatedButton(
-                  onPressed: () => {Get.toNamed("/about")},
-                  child: const Text("s")),
-              ElevatedButton(
-                  onPressed: () {
-                    Get.toNamed("/music");
-                  },
-                  child: const Text("e"))
-            ],
-          )),
-    );
+          body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  opacity: 0.1,
+                  image: NetworkImage(
+                      "https://img.sobot.com/56b093dec73c42dca06a96734ffa6258/chatres/56b093dec73c42dca06a96734ffa6258/msg/20250116/a1563905111d66f5033927df2f544f83/6bd92c745a97460aa2cb68ea70940305.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Center(
+              child: FadeTransition(
+                opacity: _animation,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            const Icon(
+                              Icons.favorite,
+                              size: 50,
+                              color: Colors.red,
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              '欢迎使用Rctool'.tr,
+                              style: const TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              '你可以听音乐和使用live2d功能'.tr,
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // 按钮1的点击事件
+                            Get.toNamed("/music");
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 15),
+                          ),
+                          child: Text('听音乐'.tr),
+                        ),
+                        const SizedBox(width: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            // 按钮2的点击事件
+                            Get.toNamed("/livetod");
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.blueAccent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 15),
+                          ),
+                          child: const Text("live2d"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
